183f0e3e69e61fa15e7a69973250d2a939713d42
Tyrone Cloma <tyrone.cloma0219@gmail.com>
2023-08-11 16:56:17 -0400

    added tailwind

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import '../app.css';
+</script>
+
+<main class="flex flex-col justify-center items-center w-full h-screen bg-neutral-300">
+    <h1 class="text-5xl font-bold hover:text-red-300">HomePage</h1>
+</main>
